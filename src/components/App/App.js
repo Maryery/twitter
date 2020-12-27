25fa64a8c72f8d418f30a8e1270b92331183d18f
Maryery Vargas Moreno <maryeryvargas@gmail.com>
2020-12-27 22:43:30 +0000

    TweetsPage component

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TweetsPage from '../Tweets/TweetsPage';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<TweetsPage />
+			</div>
+		);
+	}
+}
+
+export default App;
